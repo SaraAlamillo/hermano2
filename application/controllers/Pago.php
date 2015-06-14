851e6a0e51edaf9447cd145a5794f62743a84d56
Sara <saraalamillo93@gmail.com>
2015-06-14 15:26:32 +0200

    Añadida la parte de JavaScript al registro de pagos

--- application/controllers/Pago.php
+++ application/controllers/Pago.php
@@ -22,55 +22,51 @@ class Pago extends Main {
     }
 
     public function registra($hermano = NULL, $anio = NULL, $descripcion = NULL) {
-        echo '<pre>';
-        print_r($this->Pago_model->plazos($hermano, $descripcion));
-        echo '</pre>';
-        /* if ($this->input->post()) {
+        if ($this->input->post()) {
+            
+        } else {
+            $this->load->model('Hermano_model');
+            $hermanos = [];
 
-          } else {
-          $this->load->model('Hermano_model');
-          $hermanos = [];
+            foreach ($this->Hermano_model->lista() as $h) {
+                array_push(
+                        $hermanos, [
+                    'id' => $h->idHermano,
+                    'nombre' => $h->nombre . ' ' . $h->apellido1 . ' ' . $h->apellido2
+                        ]
+                );
+            }
 
-          foreach ($this->Hermano_model->lista() as $h) {
-          array_push(
-          $hermanos,
-          [
-          'id' => $h->idHermano,
-          'nombre' => $h->nombre . ' ' . $h->apellido1 . ' ' . $h->apellido2
-          ]
-          );
-          }
+            $this->load->model('Remesa_model');
 
-          $this->load->model('Remesa_model');
-
-          $parametros = [
-          'hermanos' => $hermanos,
-          'anios' => $this->Remesa_model->anios(),
-          'descripciones' => $this->Remesa_model->descripciones(is_null($anio) ? date('Y') : $anio),
-          'seleccionado' => [
-          'hermano' => $hermano,
-          'anio' => $anio,
-          'descripcion' => $descripcion
-          ]
-          ];
+            $parametros = [
+                'hermanos' => $hermanos,
+                'anios' => $this->Remesa_model->anios(),
+                'descripciones' => $this->Remesa_model->descripciones(is_null($anio) ? date('Y') : $anio),
+                'seleccionado' => [
+                    'hermano' => $hermano,
+                    'anio' => $anio == ''? 'Año' : $anio,
+                    'descripcion' => $descripcion
+                ]
+            ];
 
 
-          if (!is_null($hermano) and ! is_null($descripcion)) {
-          $this->load->model('Pago_model');
-          $plazos = $this->Pago_model->plazos($hermano, $descripcion);
+            if (!is_null($hermano) and ! is_null($descripcion)) {
+                $this->load->model('Pago_model');
+                $plazos = $this->Pago_model->plazos($hermano, $descripcion);
 
-          $parametros['seleccionado']['cuota1'] = $plazos->cuota1;
-          $parametros['seleccionado']['cuota2'] = $plazos->cuota2;
-          } else {
-          $parametros['seleccionado']['cuota1'] = date('d/m/Y');
-          $parametros['seleccionado']['cuota2'] = date('d/m/Y');
-          }
+                $parametros['seleccionado']['cuota1'] = $plazos->cuota1;
+                $parametros['seleccionado']['cuota2'] = $plazos->cuota2;
+            } else {
+                $parametros['seleccionado']['cuota1'] = date('d/m/Y');
+                $parametros['seleccionado']['cuota2'] = date('d/m/Y');
+            }
 
 
-          $this->load->helper('Form');
+            $this->load->helper('Form');
 
-          $this->vista($this->load->view('pago/Registro', $parametros, TRUE), 'registro');
-          } */
+            $this->vista($this->load->view('pago/Registro', $parametros, TRUE), 'registro');
+        } 
     }
 
 }
--- application/views/pago/Registro.php
+++ application/views/pago/Registro.php
@@ -1,15 +1,33 @@
-<fieldset>
-    <legend>Hermano</legend>
-    <?= crearDesplegable('hermano', $hermanos, $seleccionado['hermano'], ['nombre' => '', 'id' => '']) ?>
-</fieldset>
-<fieldset>
-    <legend>Remesa</legend>
-    <?= crearDesplegable('anio', $anios, $seleccionado['anio'], NULL, ['desc' => 'anio', 'valor' => 'anio'], TRUE) ?>
-    <?= crearDesplegable('descripcion', $descripciones, $seleccionado['descripcion'], NULL, ['desc' => 'descripcion', 'valor' => 'idRemesa'], TRUE) ?>
-</fieldset>
-<fieldset>
-    <legend>Plazos</legend>
-    <p>Cuota 1 <input type="text" name="cuota1" value="<?= $seleccionado['cuota1'] ?>" /></p>
-    <p>Cuota 2 <input type="text" name="cuota2" value="<?= $seleccionado['cuota2'] ?>" /></p>
-</fieldset>
-<input type="submit" value="Registrar" />
+<h1>Registro de pagos</h1>
+<form action="" method="POST" id="contact-form">
+    <div class="float-input">
+        <?= crearDesplegable('hermano', $hermanos, $seleccionado['hermano'], ['nombre' => 'Hermano', 'id' => ''], ['desc' => 'nombre', 'valor' => 'id'], FALSE, "id='rpHermano'") ?>
+        <span>Nombre completo</span>
+    </div>
+    <?php if ($seleccionado['hermano'] != ''): ?>
+        <div class="float-input">
+            <?= crearDesplegable('anio', $anios, $seleccionado['anio'], ['anio' => 'Año'], ['desc' => 'anio', 'valor' => 'anio'], TRUE, "id='rpAnio'") ?>
+            <span>Año</span>
+        </div>
+        <?php if ($seleccionado['anio'] != 'Año'): ?>
+            <div class="float-input">
+                <?= crearDesplegable('descripcion', $descripciones, $seleccionado['descripcion'], ['descripcion' => 'Remesa', 'idRemesa' => ''], ['desc' => 'descripcion', 'valor' => 'idRemesa'], TRUE, "id='rpDescripcion'") ?>
+                <span>Remesa</span>
+            </div>
+            <?php if ($seleccionado['descripcion'] != ''): ?>
+                <fieldset>
+                    <legend>Plazos</legend>
+                    <div class="float-input">
+                        <input type="text" name="cuota1" value="<?= $seleccionado['cuota1'] ?>" />
+                        <span>Cuota 1</span>
+                    </div>
+                    <div class="float-input">
+                        <input type="text" name="cuota2" value="<?= $seleccionado['cuota2'] ?>" />
+                        <span>Cuota 2</span>
+                    </div>
+                </fieldset>
+                <input type="submit" value="Registrar" />
+            <?php endif; ?>
+        <?php endif; ?>
+    <?php endif; ?>
+</form>
