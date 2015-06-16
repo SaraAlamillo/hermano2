73373cc3419fa08750fe65b556b8dc30873377ef
Alfonso <Alfonso@INFOR03>
2015-06-16 16:31:22 +0200

    Finalizadas las fichas de detalles

--- application/views/contacto/Detalle.php
+++ application/views/contacto/Detalle.php
@@ -1,25 +1,86 @@
-<?= anchor(site_url('contacto'), 'Volver al listado') ?>
-<h1>Detalles de contacto</h1>
-<p>Empresa: <?= $contacto->nombre_empresa ?></p>
-<p>Tratamiento: <?= $contacto->tratamiento ?></p>
-<p>Nombre: <?= $contacto->nombre ?></p>
-<p>Primer apellido: <?= $contacto->apellido1 ?></p>
-<p>Segundo apellido: <?= $contacto->apellido2 ?></p>
-<p>CIF: <?= $contacto->cif ?></p>
-<p>Tipo de vía: <?= $contacto->tipo_via ?></p>
-<p>Dirección: <?= $contacto->direccion ?></p>
-<p>Número: <?= $contacto->numero ?></p>
-<p>Piso: <?= $contacto->piso ?></p>
-<p>Puerta: <?= $contacto->puerta ?></p>
-<p>Código postal: <?= $contacto->codigo_postal ?></p>
-<p>Población: <?= $contacto->poblacion ?></p>
-<p>Móvil: <?= $contacto->movil ?></p>
-<p>Fijo: <?= $contacto->fijo ?></p>
-<p>Email: <?= $contacto->email ?></p>
-<p>Twitter: <?= $contacto->twitter ?></p>
-<p>Facebook: <?= $contacto->facebook ?></p>
-<p>Instagram: <?= $contacto->instagram ?></p>
-<p>Tipo: <?= $contacto->tipo ?></p>
-<p>Provincia: <?= $contacto->provincia ?></p>
-<?= anchor(site_url('contacto/cambio/' . $contacto->idContacto), 'Modificar') ?>
-<?= anchor(site_url('contacto/eliminar/' . $contacto->idContacto), 'Eliminar') ?>
+<h1>Vista detalla de un contacto</h1>
+<table class="table detalle">
+    <tr>
+        <th colspan="2" class="titulo">Datos personales</th>
+        <th>Tipo de contacto</th>
+        <td><?= $contacto->tipo ?></td>
+    </tr>
+    <tr>
+        <th>Empresa</th>
+        <td><?= $contacto->nombre_empresa ?></td>
+        <th colspan="2" class="titulo">Dirección</th>
+    </tr>
+    <tr>
+        <th>Tratamiento</th>
+        <td><?= $contacto->tratamiento ?></td>
+        <th>Tipo de vía</th>
+        <td><?= $contacto->tipo_via ?></td>
+    </tr>
+    <tr>
+        <th>Nombre</th>
+        <td><?= $contacto->nombre ?></td>
+        <th>Dirección</th>
+        <td><?= $contacto->direccion ?></td>
+    </tr>
+    <tr>
+        <th>Primer apellido</th>
+        <td><?= $contacto->apellido1 ?></td>
+        <th>Número</th>
+        <td><?= $contacto->numero ?></td>
+    </tr>
+    <tr>
+        <th>Segundo apellido</th>
+        <td><?= $contacto->apellido2 ?></td>
+        <th>Piso</th>
+        <td><?= $contacto->piso ?></td>
+    </tr>
+    <tr>
+        <th>CIF</th>
+        <td><?= $contacto->cif ?></td>
+        <th>Puerta</th>
+        <td><?= $contacto->puerta ?></td>
+    </tr>
+    <tr>
+        <th colspan="2" class="titulo">Contacto</th>
+        <th>Código postal</th>
+        <td><?= $contacto->codigo_postal ?></td>
+    </tr>
+    <tr>
+        <th>Móvil</th>
+        <td><?= $contacto->movil ?></td>
+        <th>Población</th>
+        <td><?= $contacto->poblacion ?></td>
+    </tr>
+    <tr>
+        <th>Fijo</th>
+        <td><?= $contacto->fijo ?></td>
+        <th>Provincia</th>
+        <td><?= $contacto->provincia ?></td>
+    </tr>
+    <tr>
+        <th>Email</th>
+        <td><?= $contacto->email ?></td>
+        <th colspan="2" class="titulo">Redes sociales</th>
+    </tr>
+    <tr>
+        <td class="vacio"></td>
+        <td class="vacio"></td>
+        <th>Twitter</th>
+        <td><?= $contacto->twitter ?></td>
+    </tr>
+    <tr>
+        <td class="vacio"></td>
+        <td class="vacio"></td>
+        <th>Facebook</th>
+        <td><?= $contacto->facebook ?></td>
+    </tr>
+    <tr>
+        <td class="vacio"></td>
+        <td class="vacio"></td>
+        <th>Instagram</th>
+        <td><?= $contacto->instagram ?></td>
+    </tr>
+</table>
+<?= anchor(site_url('contacto'), '<img alt="Volver al listado" title="Volver al listado" src="' . base_url() . 'assets/images/icons/btnAtras.png" />') ?>
+<?= anchor(site_url('contacto/cambio/' . $contacto->idContacto), '<img alt="Modificar" title ="Modificar" src="' . base_url() . 'assets/images/icons/btnEditar.png" />') ?>
+<?= anchor(site_url('contacto/elimina/' . $contacto->idContacto), '<img alt="Eliminar" title ="Eliminar" src="' . base_url() . 'assets/images/icons/btnBorrar.png" />') ?>
