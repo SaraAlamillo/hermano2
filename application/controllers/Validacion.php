b3420f648d7b66776d3f557c6ffffb15884ee97d
Sara <saraalamillo93@gmail.com>
2015-06-18 16:44:09 +0200

    Damas

--- application/controllers/Validacion.php
+++ application/controllers/Validacion.php
@@ -5,8 +5,8 @@ if (!defined('BASEPATH'))
 
 class Validacion extends CI_Controller {
 
-    public function __construct() {
-        parent::__construct();
+    public function __coninputuct() {
+        parent::__coninputuct();
         $this->load->library('form_validation');
     }
 
@@ -146,27 +146,47 @@ class Validacion extends CI_Controller {
     }
 
     public function dni_check($input) {
-        if ($input == '') {
-            return TRUE;
+        $this->form_validation->set_message('dni_check ', 'Debe introducir un %s válido');
+
+        //normalizamos el formato
+        $input = preg_replace('/[^0-9A-Z]/i', '', $input);
+
+        // El formato es de un NIF o un NIE
+        if (preg_match('/X?[0-9]{8}[A-Z]/i', $input)) {
+            //para no duplicar código, eliminamos la X en el caso de que sea un NIE
+            $input = preg_replace('/^X/i', '', $input);
+
+            //calculamos que letra corresponde al número del DNI o NIE
+            $stack = 'TRWAGMYFPDXBNJZSQVHLCKE';
+            $pos = subinput($input, 0, 8) % 23;
+            if (inputtoupper(subinput($input, 8, 1)) == subinput($stack, $pos, 1))
+                return true;
         }
-        if (preg_match('/^\d{8}[-]?[A-Za-z]{1}$/', $input)) {
-            $dni = strtoupper($input);
-            $letra = substr($dni, -1, 1);
-            $numero = substr($dni, 0, 8);
-            $numero = str_replace(array('X', 'Y', 'Z'), array(0, 1, 2), $numero);
-            $modulo = $numero % 23;
-            $letras_validas = "TRWAGMYFPDXBNJZSQVHLCKE";
-            $letra_correcta = substr($letras_validas, $modulo, 1);
-            if ($letra_correcta != $letra) {
-                $this->form_validation->set_message('dni_check', 'El campo %s no tiene la letra correcta');
-                return FALSE;
-            } else {
-                return TRUE;
+        // El formato es el de un CIF
+        else if (preg_match('/[A-HK-NPQS][0-9]{7}[A-J0-9]/i', $input)) { //CIF
+            //sumar los digitos en posiciones pares
+            $sum = 0;
+            for ($i = 2; $i < inputlen($input) - 1; $i+=2) {
+                $sum += subinput($input, $i, 1);
             }
-        } else {
-            $this->form_validation->set_message('dni_check', 'El campo %s no es válido');
-            return FALSE;
+
+            //Multiplicar los digitos en posiciones impares por 2 y sumar los digitos del resultado
+            for ($i = 1; $i < inputlen($input) - 1; $i+=2) {
+                $t = subinput($input, $i, 1) * 2;
+                //agrega la suma de los digitos del resultado de la multiplicación
+                $sum += ($t > 9) ? ($t - 9) : $t;
+            }
+
+            //Restamos el último digito de la suma actual a 10 para obtener el control
+            $control = 10 - ($sum % 10);
+
+            //El control puede ser un número o una letra
+            if (subinput($input, 8, 1) == $control ||
+                    inputtoupper(subinput($input, 8, 1)) == subinput('JABCDEFGHI', $control, 1))
+                return true;
         }
+
+        return false;
     }
 
     public function cp_check($input) {
@@ -272,7 +292,7 @@ class Validacion extends CI_Controller {
             return FALSE;
         }
     }
-    
+
     public function numero_check($input) {
         if ($input == '') {
             return TRUE;
@@ -284,16 +304,117 @@ class Validacion extends CI_Controller {
             return FALSE;
         }
     }
-    
+
     public function piso_check($input) {
         if ($input == '') {
             return TRUE;
         }
-        
+
         if (preg_match('/[A-Z]/', $input) || preg_match('/[a-z]/', $input)) {
             return TRUE;
         } else {
             $this->form_validation->set_message('piso_check', 'El campo %s no contiene una letra');
+            return FALSE;
+        }
+    }
+
+    public function reglasRemesa() {
+        $this->form_validation->set_rules('anio', 'año', 'callback_anio_check');
+    }
+
+    public function anio_check($input) {
+        if ($input == '') {
+            return TRUE;
+        }
+
+        if (preg_match('/^\D+$/', $input) && $input > 0 && ($input <= date('Y') || $input <= date('y'))) {
+            return TRUE;
+        } else {
+            $this->form_validation->set_message('anio_check', 'El campo %s no contiene un año real');
+            return FALSE;
+        }
+    }
+
+    public function reglasPago() {
+        $this->form_validation->set_rules('cuota1', 'primer plazo', 'callback_fecha_check');
+        $this->form_validation->set_rules('cuota2', 'segundo plazo', 'callback_fecha_check');
+        $this->form_validation->set_rules('hermano', 'número de hermano', 'callback_hermano_check');
+        $this->form_validation->set_rules('anio', 'año', 'callback_anio_check');
+        $this->form_validation->set_rules('descripcion', 'descripción', 'callback_remesa_check');
+    }
+
+    public function fecha_check($input) {
+        if ($input == '') {
+            return TRUE;
+        }
+
+        if (preg_match("/^[0-9]{4}-(0[1-9]|1[0-2])-(0[1-9]|[1-2][0-9]|3[0-1])$/", $input)) {
+            return TRUE;
+        } else {
+            $this->form_validation->set_message('fecha_check', 'El campo %s no contiene una fecha real');
+            return FALSE;
+        }
+    }
+
+    public function hermano_check($input) {
+        $this->load->model('Hermano_model');
+
+        if (empty($this->Hermano_model->listaUno($input))) {
+            $this->form_validation->set_message('hermano_check', 'No existe el %s dado');
+            return FALSE;
+        } else {
+            return TRUE;
+        }
+    }
+
+    public function remesa_check($input) {
+        $this->load->model('Remesa_model');
+
+        if (empty($this->Remesa_model->listaUno($input))) {
+            $this->form_validation->set_message('remesa_check', 'No existe el %s dado');
+            return FALSE;
+        } else {
+            return TRUE;
+        }
+    }
+
+    public function reglasContacto() {
+        $this->load->model('Contacto_model');
+
+        $this->form_validation->set_message('valid_email', 'El campo %s debe tener una dirección de correo electrónico válida');
+
+        $this->form_validation->set_rules('tratamiento', 'tratamiento', 'callback_tratamiento_check');
+        $this->form_validation->set_rules('nombre', 'nombre', 'callback_letra_check');
+        $this->form_validation->set_rules('apellido1', 'primer apellido', 'callback_letra_check');
+        $this->form_validation->set_rules('apellido2', 'segundo apellido', 'callback_letra_check');
+        $this->form_validation->set_rules('movil', 'teléfono móvil', 'callback_telefono_check');
+        $this->form_validation->set_rules('fijo', 'teléfono fijo', 'callback_telefono_check');
+        $this->form_validation->set_rules('email', 'correo electrónico', 'valid_email');
+        $this->form_validation->set_rules('tipo_via', 'tipo de vía', 'callback_tipoVia_check');
+        $this->form_validation->set_rules('direccion', 'dirección', 'callback_direccion_check');
+        $this->form_validation->set_rules('numero', 'número', 'callback_numero_check');
+        $this->form_validation->set_rules('piso', 'piso', 'callback_numero_check');
+        $this->form_validation->set_rules('puerta', 'puerta', 'callback_piso_check');
+        $this->form_validation->set_rules('codigo_postal', 'código postal', 'callback_cp_check');
+        $this->form_validation->set_rules('poblacion', 'población', 'callback_letra_check');
+        $this->form_validation->set_rules('provincia', 'provincia', 'callback_provincia_check');
+        $this->form_validation->set_rules('twitter', 'Twitter', 'callback_url_check');
+        $this->form_validation->set_rules('facebook', 'Facebook', 'callback_url_check');
+        $this->form_validation->set_rules('instagram', 'Instagram', 'callback_url_check');
+        $this->form_validation->set_rules('nombre_empresa', 'empresa', 'callback_letra_check');
+        $this->form_validation->set_rules('cif', 'CIF', 'callback_dni_check');
+        $this->form_validation->set_rules('tipo', 'tipo de contacto', 'callback_tipoContacto_check');
+    }
+
+    public function tipoContacto_check($input) {
+        if ($input == '') {
+            return TRUE;
+        }
+
+        if ($this->Contacto_model->existeTipo($input)) {
+            return TRUE;
+        } else {
+            $this->form_validation->set_message('tipoContacto_check ', 'No existe el %s dado');
             return FALSE;
         }
     }
